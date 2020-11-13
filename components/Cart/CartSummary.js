e25827632b08d0c37e4f40f45de9f621a07c43ae
Chaise Matev <aj_fl81@msn.com>
2020-11-13 13:55:15 -0500

    created cart page including list and summary

--- components/Cart/CartSummary.js
+++ components/Cart/CartSummary.js
@@ -1,5 +1,21 @@
+const { Divider, Segment, Button } = require('semantic-ui-react');
+
 function CartSummary() {
-  return <>CartSummary</>;
+  return (
+    <>
+      <Divider>
+        <Segment clearing size="large">
+          <strong>Sub total:</strong>$0.00
+          <Button
+            icon="cart"
+            color="teal"
+            floated="right"
+            content="Checkout"
+          ></Button>
+        </Segment>
+      </Divider>
+    </>
+  );
 }
 
 export default CartSummary;

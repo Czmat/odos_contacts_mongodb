dcb766d26961c59b58261b53325069d297024aad
Chaise Matev <aj_fl81@msn.com>
2020-11-14 21:01:57 -0500

    added singup function with route

--- pages/signup.js
+++ pages/signup.js
@@ -1,7 +1,10 @@
+import { useEffect, useState } from 'react';
 import { Message, Form, Segment, Button, Icon } from 'semantic-ui-react';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
+import axios from 'axios';
 import catchErrors from '../utils/catchErrors';
+import baseUrl from '../utils/baseUrl';
+import { handleLogin } from '../utils/auth';
 
 const INITIAL_USER = {
   name: '',
@@ -32,6 +35,10 @@ function Signup() {
       setError('');
       console.log(user);
       // make request to signup user
+      const url = `${baseUrl}/api/signup`;
+      const payload = { ...user };
+      const response = await axios.post(url, payload);
+      handleLogin(response.data);
     } catch (error) {
       catchErrors(error, setError);
     } finally {

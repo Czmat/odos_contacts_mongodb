8745d4c9a772d4707f16f154fd92b2b68d41e5e9
Chaise Matev <aj_fl81@msn.com>
2020-12-02 12:38:27 -0500

    production url change

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://odoscontacts.vercel.sh'
+    ? 'https://odos-contacts.vercel.app'
     : 'http://localhost:3000';
 
 export default baseUrl;
